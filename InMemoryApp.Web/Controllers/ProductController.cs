a5171f2ad07ec3dd3118af69adb4e2bf2ad18c9b
OnurEgemen <onur.egemen@yahoo.com>
2024-12-25 18:15:24 +0300

    Complex Types Caching Has Been Added

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryApp.Web.Controllers
@@ -24,30 +25,27 @@ namespace InMemoryApp.Web.Controllers
             //}
 
             // 2. YOL
-            if(!_memoryCache.TryGetValue("zaman",out string zamanCache))
+            if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
             {
-                
-                
-                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
-                options.AbsoluteExpiration = DateTime.Now.AddMinutes(5);
 
-
-                options.SlidingExpiration = TimeSpan.FromSeconds(10);
-                options.Priority = CacheItemPriority.Low;
-
-                options.RegisterPostEvictionCallback((key, value, reason, state) =>
-                {
-                    _memoryCache.Set("callback", $"{key}->{value} => sebep:{reason}");
-                });
-
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),options);
             }
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(5);
 
 
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            options.Priority = CacheItemPriority.Low;
 
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}->{value} => sebep:{reason}");
+            });
 
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
+            Product p = new Product { Id = 1, Name = "Kalem", Price = 100 };
 
+            _memoryCache.Set<Product>("product:1", p);
 
             return View();
         }
@@ -69,6 +67,8 @@ namespace InMemoryApp.Web.Controllers
             _memoryCache.TryGetValue("callback", out string callback);
             ViewBag.zaman = zamanCache;
             ViewBag.callback = callback;
+
+            ViewBag.product = _memoryCache.Get<Product>("product:1");
 
 
             return View();
